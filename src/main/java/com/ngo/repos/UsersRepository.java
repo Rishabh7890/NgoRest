55f5ddabf86e363843ac671e19e108543a05adc8
rishabh <rishabh7890@gmail.com>
2020-01-31 12:48:15 -0500

    repos

---
+++ src/main/java/com/ngo/repos/UsersRepository.java
@@ -0,0 +1,11 @@
+package com.ngo.repos;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.ngo.beans.Users;
+
+public interface UsersRepository extends MongoRepository<Users, String> {
+
+	public Users findByUserEmail(String userEmail);
+
+}
